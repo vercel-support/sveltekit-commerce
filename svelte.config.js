b09ba215df4a33092b26bca581d442f020befe5a
n-vigier <1329981+n-vigier@users.noreply.github.com>
2025-03-01 16:54:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $components: 'src/components',
+      $utils: 'src/utils'
+    }
+  },  
+};
+
+export default config;
